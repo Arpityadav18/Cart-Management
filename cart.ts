79042d872b03aad4ac23331364508fdba5604604
Arpit Yadav <112938376+Arpityadav18@users.noreply.github.com>
2023-05-12 11:47:12 +0530

    first commit

---
+++ cart.ts
@@ -0,0 +1,20 @@
+var product = new Map();
+product.set(1,"Laptop");
+product.set(2,"Desktop");
+product.set(3,"Mobile");
+product.set(4,"TV");
+product.set(5,"Cooler");
+
+// Show All Product
+console.log("Available Products :")
+console.warn(product);
+console.warn("Product Cooler is Deleted"+" "+product.delete(5)); // Remove Product
+console.warn(product);
+console.warn("Product TV is Deleted"+" "+product.delete(4)); // Remove Product
+console.warn(product);
+
+
+
+
+
+
